6cca39fafde4d5496b4511b3875a4c300050d5e6
Dipak Kumar Yadav <ydipakkumar360@gmail.com>
2026-09-15 13:12:29 +0545

    update allow_host setting

--- eventManager/settings.py
+++ eventManager/settings.py
@@ -28,16 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--x-u8*y+o2!p_=yl(&8%t*2o&8w7qsin2r4drb+wki#50g-w4u"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "event-planner-lr7hpxzex-dpak.vercel.app",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
